a706d02fe73c9bf9941d10cdb7188c8422316cec
Glorifind <adam.lakomy@gmail.com>
2017-01-12 22:57:51 +0100

    first commit

---
+++ app/src/main/java/com/pintabestbeers/Main2Activity.java
@@ -0,0 +1,21 @@
+package com.pintabestbeers;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    public void openBeerInfo(View view) {
+        Intent intent = new Intent(Main2Activity.this, Main3Activity.class);
+        startActivity(intent);
+    }
+}
+
---
+++ app/src/main/java/com/pintabestbeers/MainActivity.java
@@ -0,0 +1,24 @@
+package com.pintabestbeers;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void moreThan18(View view) {
+        Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+        startActivity(intent);
+    }
+
+    public void lessThan18(View view) {
+        this.finishAffinity();
+    }
+}
